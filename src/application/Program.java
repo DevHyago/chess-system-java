1858360c373afd62f2da06ca3c09b344113eac6e
DevHyago <dev.hyago@outlook.com>
2021-07-19 07:48:55 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello word!");
+		Position pos = new Position(3, 4);
+		
+		System.out.println(pos);
 
 	}
 
